5165da5eecf46bba974ca757d930e19d6bf2079a
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:57:12 +0530

    Add files via upload

---
+++ 08_operator_overloading.py
@@ -0,0 +1,18 @@
+class Number:
+     def __init__(self, num):
+          self.num = num
+
+     def __add__(self, num2):
+          print("Lets add ")
+          return self.num + num2.num
+     
+     def __mul__(self, num2):
+          print("Lets multiply ")
+          return self.num * num2.num
+ 
+n1 = Number(6)
+n2 = Number(4)
+sum = n1 + n2
+print(sum)
+mul = n1 * n2
+print(mul)

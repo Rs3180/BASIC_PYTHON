67ce31e8045d10eb77ffecc2844ea9c208d6f609
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:08:46 +0530

    Add files via upload

---
+++ 1_variable.py
@@ -0,0 +1,18 @@
+a = "4"
+b = 7.5
+c = 8
+d = None
+
+#printinf the variables
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#printing the type of variables
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))

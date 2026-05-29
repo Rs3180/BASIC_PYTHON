0b63b435cf6bbb703386fb1a22d0f4edc7123c66
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:51:31 +0530

    Add files via upload

---
+++ 13_05_Practice_set.py
@@ -0,0 +1,31 @@
+class Train:
+    def __init__(self, name, fare, seats):
+          self.name = name 
+          self.fare = fare 
+          self.seats = seats   
+
+    def getStarts(self):
+         print(f"The name pf the train is {self.name}")
+         print(f"The seats available in the train are {self.seats}")   
+
+    def fareInfo(self):
+         print(f"The price of the ticket is: Rs {self.fare}")
+
+    def bookTicket(self):
+           if(self.seats>0):
+                print(f"Your ticket has been booked! Your seat number is {self.seats}")
+                self.seats = self.seats - 1
+           else:
+                print("Sorry this train is full! kindly try in tatkal")
+
+    def cancelTicket(self, seatNo):
+           self.seatNo = seatNo 
+           
+intercity = Train("Intercity Express: 14015", 90,2)
+intercity.getStarts()
+intercity.fareInfo()
+intercity.bookTicket()
+intercity.bookTicket()
+intercity.bookTicket()
+intercity.getStarts()
+# intercity.cancelTicket()

8ad2900eca70250a448f41706acf391e2f069d93
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:12:17 +0530

    Add files via upload

---
+++ 02_string_slicing.py
@@ -0,0 +1,12 @@
+greeting = "Good Morning, "
+# name = "Raj"
+# print(type(name))
+
+# Concatenating two strings
+# c = greeting + name 
+# print(c)
+name = "Singhraj"
+print(name[7])
+# name[3] ="d"  ---> Does not work
+
+print(name[1:4])

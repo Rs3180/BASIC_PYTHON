f072a28f882bdd04d2ae97c664594af8e71c9bd3
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:11:39 +0530

    Add files via upload

---
+++ 10_pr_06_calculate_square_number.py
@@ -0,0 +1,9 @@
+# a = input("Enter a number length : ")
+# b = input("Enter a number breadth : ")
+# area = int(a) * int(b)
+# print("The area of square is ", area )
+
+
+a = input("Enter a number : ")
+square = int(a) * int(a)
+print("The square of a number is ", square)

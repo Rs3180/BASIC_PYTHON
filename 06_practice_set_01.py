1dc4c1f7be8976231146d6ccce6180e8ea442ccb
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:14:31 +0530

    Add files via upload

---
+++ 06_practice_set_01.py
@@ -0,0 +1,17 @@
+a = int(input("Enter Number 1: "))
+b = int(input("Enter Number 2: "))
+c = int(input("Enter Number 3: "))
+d = int(input("Enter Number 4: "))
+
+if(a>d):
+          f1 = a
+else:
+          f1 = d
+if(b>c):
+          f2 = b
+else:
+          f2 = c
+if(f1>f2):
+          print(f1, "is graterest")
+else:
+                    print(f2, "greater number")

95bc627e2ef6e69abaefd2ef813a0d91139d4365
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:12:54 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,34 @@
+a = 15
+
+# 1. if-elif-else ladder in python
+# if(a<3):
+#           print("The value of a is greater than 3")
+# elif(a<7):
+#           print("The value of a is greater than 7")
+# elif(a>13):
+#           print("The value of a is greater than 13")
+# elif(a>17):
+#           print("The value of a is greater than 17")
+# elif(a>20):
+#           print("The value of a is greater than 20") 
+# else:
+#           print("The value of a is greater than 3 or 7")   
+
+# print("Done")     
+
+
+# 2. multiple if statements
+if(a<3):
+          print("The value of a is greater than 3")
+if(a<7):
+          print("The value of a is greater than 7")
+if(a>13):
+          print("The value of a is greater than 13")
+if(a>17):
+          print("The value of a is greater than 17")
+if(a>12):
+          print("The value of a is greater than 12") 
+else:
+          print("The value of a is greater than 3 or 7")   
+
+print("Done")  

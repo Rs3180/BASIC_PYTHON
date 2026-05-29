a09a234e0eeed251b3be2fb7f504f68168578426
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:32:37 +0530

    Add files via upload

---
+++ 01_octa_hexdecimail.py
@@ -0,0 +1,36 @@
+print(0o123)
+print(0x123)
+#scientific notation 
+print(3E8)
+print(6.62607E-34) #6.62607 x 10^-34
+print(0.000000000000000000001)
+
+##string
+print("I like \"monty python\"")
+print('I like "Monty Python"')
+print('I\'m Monty Python.')
+
+##Boolean values
+print(True>False)
+print(True<False)
+
+####
+#"I'm"
+#""learing""
+#"""Python"""
+print("\"I'm\"")
+print('\"\"learing\"\"')
+print('\"\"\"Python\"\"\"')
+
+
+##Basic Operators
+
+# print(2 ** 3)
+# Print(2 ** 3.)
+# print(2. ** 3)
+# print(2. ** 3.)
+
+
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+    

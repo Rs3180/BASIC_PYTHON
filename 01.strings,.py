6ba69366b38aa9b0422a4b731ffe92c32095bb05
Raj <rajsinghlkr8@gmail.com>
2026-05-29 08:12:02 +0530

    Add files via upload

---
+++ 01.strings,.py
@@ -0,0 +1,10 @@
+# import os 
+# import flask
+# print("HELLO WORLD")
+
+
+# b = "Raj's" #---> use this if you have single quotes in your strings
+# b = 'Raj"s' #---> use this if you have double quotes in your strings
+b = '''Raj"s and
+   Raj's''' #---> use this if you have both single and double quotes in your strings
+print(b)
